36abb633b92ea1a812e61144fb923c5018cd5bcc
birdroid <birdroid@126.com>
2012-11-18 22:12:31 +0800

    add severTest

--- server.js
+++ server.js
@@ -3,25 +3,38 @@ var url = require("url")
 var weibo = require("weiboapi")
 var fs = require("fs")
 var api = null
+var visitor = 0
+var servTest = function()
+{
+  var mem = process.memoryUsage()
+  if(visitor++ % 10 ==0 )
+    console.log('visitor : ' + visitor +
+                '  heapTotal : ' + mem.heapTotal +
+                '  heapUsed : '  + mem.heapUsed)
+ 
+}
 
-weibo.login('birdroid@126.com','xxx',function (err,API){
+weibo.login('birdroid@126.com','xx',function (err,API){
   if(!err){
       api = API
+  }else{
+    console.log('login fail')}
   }
-})
-http.createServer(function(requst,response){
-  var uri = url.parse(requst.url,true)
+)
 
+http.createServer(function(requst,response){
+  
+  servTest()
+  
+  var uri = url.parse(requst.url,true)
+  process.heapTotal,process.heapUsed
   if(uri.pathname == '/getuser'){
-    var expTime = Date.now() + 24*60*60*1000
     response.writeHeader(200,{"Content-type":"application/json;charset=utf-8",
-      Server:'nodejs',
-      "Cache-control": 'max-age=' + 24*60*60,
-      Expires:(new Date(expTime)).toGMTString()
+                                      Server:'nodejs'
     })
     if(api){
       api.searchUser(uri.query,function(err,result){
-        response.write(JSON.stringify(result))
+        response.write(JSON.stringify(result || ''))
         response.end()
       })
     }else{
@@ -29,7 +42,7 @@ http.createServer(function(requst,response){
       response.end()
     }
   }else if(uri.pathname == '/'){
-    var content = fs.readFileSync('beauty/static/index.html', 'utf8')
+    var content = fs.readFileSync('static/index.html', 'utf8')
     response.writeHeader(200,{"Content-type":"text/html;charset=utf-8"})
     response.write(content)
     response.end()
@@ -37,8 +50,9 @@ http.createServer(function(requst,response){
     response.write('address wrong')
     response.end()
   }
+  
 }).listen(8888)
 
 process.on('uncaughtException',function(err){
-  console.log(err)
+  console.log(err.stack)
 })
